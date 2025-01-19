8e2418b1367c8568be62398d7cdfdc43bff448ef
faizi729 <f68857440@gmail.com>
2025-01-19 23:53:31 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion"
 import 'react-vertical-timeline-component/style.min.css';
 import {styles} from '../style';
 import {experiences} from '../constants';
-import {SectionWrapper} from '../hoc';
+import {SectionWrapper} from '../HOC';
 import { textVariant } from "../utils/motion";
 
 
